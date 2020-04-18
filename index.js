85c139e231f01a05a70117d4e7c21f58b65ef534
HelenHMaher <helenharlanmaher@gmail.com>
2020-04-18 13:25:33 +0200

    fetch data

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const width = 800,
+const url =
+  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
+
+const width = 2000,
   height = 500,
   padding = 60;
 
@@ -7,3 +10,14 @@ const svgContainer = d3
   .append("svg")
   .attr("width", width)
   .attr("height", height);
+
+fetch(url)
+  .then((response) => response.json())
+  .then((dataset) => {
+    console.log(dataset);
+    svgContainer
+      .append("text")
+      .attr("x", 100)
+      .attr("y", 100)
+      .text(JSON.stringify(dataset));
+  });
